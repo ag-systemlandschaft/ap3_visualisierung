8bf9e227c5c6736895e39c1a3bc822f50147675c
vlg <verena.glatt@softwareschneiderei.de>
2024-12-11 13:29:52 +0100

    main: Show data exchange descriptions

--- index.js
+++ index.js
@@ -124,11 +124,11 @@ function init(data) {
 
     d3.select(".infoText")
         .selectAll("span")
-        .data([{"id" : "startupText", "description" :"Diese Visualisierung zeigt Importdatenflüsse auf Basis von Echtdaten der ersten Fragerunde. Für die erste prototypische Visualisierung wurde nur ein Bruchteil der vorhandenen Daten ausgewertet. Aufgrund der High-Level-Darstellung gibt die Visualisierung aktuell keine Auskunft über potentielle Doppellieferungen. Die Umsetzung des im AG-Kontext erarbeiteten Visualisierungskonzepts ist aktuell noch in Arbeit.", "group": ""}].concat(systems))
+        .data(dataExchanges.flatMap(dataExchange => dataExchange["processes"]).concat([{"id" : "startupText", "description" :"Diese Visualisierung zeigt Importdatenflüsse auf Basis von Echtdaten der ersten Fragerunde. Für die erste prototypische Visualisierung wurde nur ein Bruchteil der vorhandenen Daten ausgewertet. Aufgrund der High-Level-Darstellung gibt die Visualisierung aktuell keine Auskunft über potentielle Doppellieferungen. Die Umsetzung des im AG-Kontext erarbeiteten Visualisierungskonzepts ist aktuell noch in Arbeit.", "group": ""}]).concat(systems))
         .enter()
         .append("span")
         .attr("class", "infoTextSpan")
-        .attr("info-id", d => d.id)
+        .attr("info-id", d => d.id === undefined ? d.name : d.id)
         .html(function(d) { return d.description });
 
     setListeners();
@@ -150,11 +150,12 @@ function setListeners() {
     dataExchanges.forEach(dataExchange => dataExchange.addEventListener("click", function(e) {
         console.log("Clicked element:", e.target);
         const linkData = e.target.__data__; // Hier wird das Datenobjekt des Link-Elements abgerufen
-        const linkid = linkData.id;
+        console.log(linkData);
+        const linkids = linkData.processes.map(d => d.name);
         let infoTexts = document.querySelectorAll(".infoTextSpan");
         infoTexts.forEach(span => span.style.display = "none");
-        console.log("info-id:", linkid);
-        document.querySelector(".infoTextSpan[info-id='" + linkid + "']").style.display = "initial";
+        console.log("info-ids:", linkids);
+        linkids.forEach(id => document.querySelector(".infoTextSpan[info-id='" + id + "']").style.display = "initial");
     }));
 }
 
