f615b933fa2fa9236be2109628eedd186e5974da
vlg <verena.glatt@softwareschneiderei.de>
2024-12-17 09:28:44 +0100

    AA20: Add infobox for links and refactore infobox creation

--- assets/index.js
+++ assets/index.js
@@ -11,17 +11,18 @@ function init(data) {
         systems = data.systems,
         filters = data.propertyCategories
 
+    filters.sort((a, b) => a.name.localeCompare(b.name))
+
     //d3 Simulation search for id. Check if another tag can be defined as identifier
     systems.forEach(d => {d.id = d.shortName});
 
-    const infoTexts = [{
-        "id": "startupText",
-        "description": "Diese Visualisierung zeigt Importdatenflüsse auf Basis von Echtdaten der ersten Fragerunde. Für die erste prototypische Visualisierung wurde nur ein Bruchteil der vorhandenen Daten ausgewertet. Aufgrund der High-Level-Darstellung gibt die Visualisierung aktuell keine Auskunft über potentielle Doppellieferungen. Die Umsetzung des im AG-Kontext erarbeiteten Visualisierungskonzepts ist aktuell noch in Arbeit."
-    }].concat(systems).concat(dataExchanges.flatMap(dataExchange => dataExchange["processes"]))
+    const infoText = document.querySelector(".infoText");
+    infoText.innerHTML = `
+        Diese Visualisierung zeigt Importdatenflüsse auf Basis von Echtdaten der ersten Fragerunde. Für die erste prototypische Visualisierung wurde nur ein Bruchteil der vorhandenen Daten ausgewertet. Aufgrund der High-Level-Darstellung gibt die Visualisierung aktuell keine Auskunft über potentielle Doppellieferungen. Die Umsetzung des im AG-Kontext erarbeiteten Visualisierungskonzepts ist aktuell noch in Arbeit.
+    `
 
     const svg = d3.select("svg");
-    initGraph(svg, systems, dataExchanges);
-    initInfoText(infoTexts)
+    initGraph(svg, systems, dataExchanges, filters);
 
     setFilters(filters);
 }
