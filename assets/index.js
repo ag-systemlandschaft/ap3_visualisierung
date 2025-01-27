4f515e32296358c060ef79f9b082733453ddad6d
vlg <verena.glatt@softwareschneiderei.de>
2025-01-27 13:47:33 +0100

    FIZE-585: Show default info when click on free space

--- assets/index.js
+++ assets/index.js
@@ -13,18 +13,7 @@ function init(data) {
     systems.forEach(d => d.id = d.shortName);
 
     dataExchanges.forEach(d => d.processes.forEach(process => process.active = true));
-
-    const infoText = document.querySelector(".infoText");
-    const heading = document.createElement('h3');
-    heading.className = 'title is-5 mb-2';
-    heading.textContent = 'Information';
-    infoText.appendChild(heading);
-    infoText.appendChild(document.createTextNode(`
-        Diese Visualisierung zeigt Importdatenflüsse auf Basis von Echtdaten der ersten Fragerunde. 
-        Für die erste prototypische Visualisierung wurde nur ein Bruchteil der vorhandenen Daten ausgewertet. 
-        Aufgrund der High-Level-Darstellung gibt die Visualisierung aktuell keine Auskunft über potentielle Doppellieferungen. 
-        Die Umsetzung des im AG-Kontext erarbeiteten Visualisierungskonzepts ist aktuell noch in Arbeit.
-    `));
+    setDefaultInfoText();
 
     const svg = d3.select("svg");
     svg.append("text")
